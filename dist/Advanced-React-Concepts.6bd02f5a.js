084149cb5fa1a176bfc3f1306886292ee37273ce
Rakshith <harshith.shetty2002@gmail.com>
2025-07-04 12:31:59 +0530

    learning

---
+++ dist/Advanced-React-Concepts.6bd02f5a.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm a h2 tag")
+    ])
+]);
+// const heading = React.createElement("h1", {}, "Hello World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
+
+//# sourceMappingURL=Advanced-React-Concepts.6bd02f5a.js.map
