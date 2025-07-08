81480c5d840bde10251a152db940057716ef8072
Harshith KV <140829094+Harshithshetty07@users.noreply.github.com>
2025-07-08 15:09:38 +0530

    learning on

--- App.js
+++ App.js
@@ -2,30 +2,30 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement(
-        "div", 
-        {id: "parent"}, 
-        [
-            React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
             "div",
-            {id: "child"},
+            { id: "child" },
             [React.createElement("h1", {}, "I'm h1 tags"),
-            React.createElement("h2", {} , "I'm a h2 tag"),
-        ]
+            React.createElement("h2", {}, "I'm a h2 tag"),
+            ]
         ),
         React.createElement(
             "div",
-            {id: "child2"},
+            { id: "child2" },
             [React.createElement("h1", {}, "I'm h1 tag"),
-            React.createElement("h2", {} , "I'm a h2 tag"),
-        ]
+            React.createElement("h2", {}, "I'm a h2 tag"),
+            ]
         )
-        ]
-    )
+    ]
+)
 
-    // const heading = React.createElement("h1", {}, "Hello World!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
+// const heading = React.createElement("h1", {}, "Hello World!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    
+
 console.log(parent)
 
-    root.render(parent)
+root.render(parent)
